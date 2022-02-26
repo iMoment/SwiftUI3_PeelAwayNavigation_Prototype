fd7426a542323625182062271824bcaf018158ef
Stanley Pan <imoment.developer@gmail.com>
2022-02-26 15:58:54 -0500

    Initial Commit

---
+++ LiquidSwipe/LiquidSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeApp.swift
+//  LiquidSwipe
+//
+//  Created by Stanley Pan on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
